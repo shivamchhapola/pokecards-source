41756ea79a56395b70fd9745b6c563cab716e5e4
Shivam Chhapola <shivam.chhapola.007@gmail.com>
2022-07-07 09:00:29 +0530

    Almost Complete

--- src/JS/Card.js
+++ src/JS/Card.js
@@ -2,14 +2,37 @@ import '../CSS/Card.css';
 import ProgressBar  from './ProgressBar';
 import { ReactComponent as D1 } from '../images/Design.svg';
 
-function Card({PokeName, PokeImg, hp, atk, def, spd}) {
+function Card({ pokemon }) {
+  const designColor = {
+    "grass": "#9EFF7B",
+    "normal": "#72535B",
+    "fighting": "",
+    "flying":"",
+    "poison":"",
+    "ground":"",
+    "rock":"",
+    "bug":"",
+    "ghost":"",
+    "steel":"",
+    "fire":"",
+    "water":"",
+    "shadow":"",
+    "electric":"",
+    "psychic":"",
+    "ice":"",
+    "dragon":"",
+    "dark":"",
+    "fairy":"",
+    "unknown":"",
+  }
+
   return(
     <div className="Card">
       <div className="Design">
-        <D1 className="D1" fill="#9EFF7B"/>
+        <D1 className="D1" fill={designColor[pokemon[6]]}/>
       </div>
-      <img src={PokeImg} className="PokeImg" alt={PokeName} />
-      <div className="PokeName">{PokeName}</div>
+      <img src={pokemon[1]} className="PokeImg" alt={pokemon[0]} />
+      <div className="PokeName">{pokemon[0]}</div>
       <div className="Stats">
         <div className="Labels">
           <label htmlFor="hp" style={{ color: '#F37474' }}>hp</label>
@@ -18,10 +41,10 @@ function Card({PokeName, PokeImg, hp, atk, def, spd}) {
           <label htmlFor="hp" style={{ color: '#D37CF1' }}>spd</label>
         </div>
         <div className="ProgressBars">
-          <ProgressBar bgcolor="#F37474" stat="49" height="0.85rem"/>
-          <ProgressBar bgcolor="#5DB4F2" stat="75" height="0.85rem"/>
-          <ProgressBar bgcolor="#F8A04F" stat="89" height="0.85rem"/>
-          <ProgressBar bgcolor="#D37CF1" stat="125" height="0.85rem"/>
+          <ProgressBar bgcolor="#F37474" stat={pokemon[2]} height="0.85rem"/>
+          <ProgressBar bgcolor="#5DB4F2" stat={pokemon[3]} height="0.85rem"/>
+          <ProgressBar bgcolor="#F8A04F" stat={pokemon[4]} height="0.85rem"/>
+          <ProgressBar bgcolor="#D37CF1" stat={pokemon[5]} height="0.85rem"/>
         </div>
       </div>
     </div>
--- src/JS/Home.js
+++ src/JS/Home.js
@@ -1,7 +1,19 @@
 import '../CSS/Home.css';
 import Card from './Card.js';
+import { useState } from 'react';
+import axios from 'axios';
 
 function Home() {
+  const [searchTxt, setSearchTxt] = useState("");
+  const [pokemon, setPokemon] = useState(['Search', 'https://www.svgrepo.com/show/14071/search.svg', 0, 0, 0, 0], 'unknown');
+
+  const handleSearch = e => {
+    e.preventDefault();
+    axios.get("https://pokeapi.co/api/v2/pokemon/" + searchTxt).then(res => {
+      setPokemon([res.data.name, res.data.sprites.other['official-artwork'].front_default, res.data.stats[0].base_stat, res.data.stats[1].base_stat, res.data.stats[2].base_stat, res.data.stats[5].base_stat, res.data.types[0].type.name]);
+    });
+  }
+
   return (
     <div className="Body">
       <div className="Main">
@@ -9,10 +21,10 @@ function Home() {
           <h1> PokeCards </h1>
         </header>
         <div className="SearchBar">
-          <input type="text" className="SearchText" placeholder="Pokemon ID/Name"/>
-          <button className="SearchButton">Go</button>
+          <input type="text" className="SearchText" placeholder="Pokemon ID/Name" onChange={(e) => setSearchTxt(e.target.value)}/>
+          <button className="SearchButton" onClick={(e) => handleSearch(e)}>Go</button>
         </div>
-        <Card PokeName="Bulbasaur" PokeImg="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+        <Card pokemon={pokemon} />
       </div>
       <footer className="Footer">©Shivam Chhapola</footer>
     </div>
